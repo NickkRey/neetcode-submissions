2b1248655f313b965545b700c49c3fa2a5a875b8
NickkRey <nickk26rey@gmail.com>
2026-08-08 21:00:29 +0700

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> hashset;
+        for(int i=0;i<nums.size();i++){
+            if(hashset.count(nums[i])){
+                return true;
+            }
+            hashset.insert(nums[i]);
+        }
+        return false;
+    }
+};
+
+
