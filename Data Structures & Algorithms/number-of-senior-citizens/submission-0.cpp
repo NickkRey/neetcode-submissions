4d6d9570e68dfd432595b90c0ee3d176eea5a9fe
NickkRey <nickk26rey@gmail.com>
2026-08-21 16:03:27 +0700

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int result=0;
+        for(const string& d : details){
+            int ten = d[11]-'0';
+            int one = d[12]-'0';
+            int age = one + ten * 10;
+            if(age>60){
+                result++;
+            }
+        }
+        return result;
+    }
+};
